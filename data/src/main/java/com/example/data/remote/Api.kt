cf44027f2a8e09658a34f42dbf0f8fcac846d88d
Abdulrahman1890 <abdulrahmanmm2011@hotmail.com>
2023-06-17 18:55:27 +0300

    search

--- data/src/main/java/com/example/data/remote/Api.kt
+++ data/src/main/java/com/example/data/remote/Api.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 //base url of website
 private const val BASE_URL =
-    "https://40f5-105-37-90-242.ngrok-free.app/api/"
+    "https://9c44-105-41-218-121.ngrok-free.app/api/"
 
 //moshi build which we will use to convert json to object kotlin
 private val moshi = Moshi.Builder()
